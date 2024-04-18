4a30ff9da6592a080b343022c6c5c493c1a04232
ing_oscargdg <ing.oscargdg@gmail.com>
2024-04-18 08:16:27 -0600

    Agregue programa para determinar si un numero es primo

---
+++ 01_Numero_Primo/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Introdusca el numero que desea evaluar si es numero primo");
+String numberEvaluar = Console.ReadLine();
+int numericValue;
+bool isNumber = int.TryParse(numberEvaluar, out numericValue);
+
+if(isNumber){
+    int a = 0;
+    for(int i = 1;i <= numericValue;i++){
+        if(numericValue % i == 0){
+            a++;
+        }
+    }
+    if(a != 2){
+        Console.WriteLine("El numero ['" + numberEvaluar + "'] no es primo");    
+    }else{
+        Console.WriteLine("El numero ['" + numberEvaluar + "'] es primo");    
+    }
+}
+else{
+    Console.WriteLine("El dato introducido ['" + numberEvaluar + "'] no es un numero valido");
+}
+
